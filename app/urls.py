e607f00b0226bce9fce20f368fa03ebdfcb86f79
caiseyann <caiseyann4@gmail.com>
2020-10-24 17:06:12 +0300

    importing modules for urls.py

---
+++ app/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
+
