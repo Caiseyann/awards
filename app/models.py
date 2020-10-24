6e3ad80940047579dbd9ddd339183141b6a61cb6
caiseyann <caiseyann4@gmail.com>
2020-10-24 15:19:52 +0300

    class Profile changes made

--- app/models.py
+++ app/models.py
@@ -6,3 +6,7 @@ import numpy as np
 
 # Create your models here.
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    profile_photo = models.ImageField(upload_to='profiles/',null=True)
+    bio = models.CharField(max_length=240, null=True)
+    phone = models.PositiveIntegerField(default=0)
