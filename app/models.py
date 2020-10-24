e3e48a5e495172fa9bd39b7338585a0b0363fc66
caiseyann <caiseyann4@gmail.com>
2020-10-24 15:21:37 +0300

    class Project created

--- app/models.py
+++ app/models.py
@@ -26,3 +26,12 @@ class Profile(models.Model):
         profile = Profile.objects.all()
 
         return profile
+
+
+class Project(models.Model):
+    posted_by = models.ForeignKey(User, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE,null=True)
+    title = models.CharField(max_length=100, null=True)
+    project_image = models.ImageField(upload_to='projects/',null=True)
+    description = models.TextField(null=True)
+    project_link = models.TextField(null=True)
